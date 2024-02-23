214fb855b60fd0856f24053a42342478362f9f87
AryanshRana <ranaaryansh12@gmail.com>
2024-02-24 01:09:18 +0530

    using Route Groups

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+// if we don't want to show auth(folder) in our url and directly want to go to login and register page and also have a layout file common for both , we can not do that , so to achieve that we have concepts caled routes groups
+const Page = () => {
+    return(
+        <div>
+            LoginPage
+        </div>
+    );
+}
+export default Page;
